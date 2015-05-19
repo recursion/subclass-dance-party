0a43d75ce63794a120814afc91ef5b462d18b58b
Michael Symmes <msymmes@gmail.com>
2015-05-18 20:00:59 -0400

    Added two new dancer files

--- src/dancer.js
+++ src/dancer.js
@@ -6,26 +6,29 @@ var Dancer = function(top, left, timeBetweenSteps){
   this.timeBetweenSteps = timeBetweenSteps;
 
   // this.step();
+  Dancer.prototype.step.call(this);
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
 
   // this.setPosition(top, left);
+  Dancer.prototype.setPosition.call(this, top, left);
+
 };
 
-  Dancer.prototype.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step, this.timeBetweenSteps);
-  };
+Dancer.prototype.step = function(){
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
 
-  Dancer.prototype.setPosition = function(top, left){
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
+Dancer.prototype.setPosition = function(top, left){
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
   };
+  this.$node.css(styleSettings);
+};
